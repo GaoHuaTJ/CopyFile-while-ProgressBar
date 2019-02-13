a58ed7f625234d84d22752d3b52d6a45e4348d7f
qidaxiang66@icloud.com <qidaxiang66@icloud.com>
2019-02-13 11:07:54 +0800

    2.0

--- 复制文件时显示复制进度/Form1.Designer.cs
+++ 复制文件时显示复制进度/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxOrigin = new System.Windows.Forms.TextBox();
+            this.textBoxGoalFile = new System.Windows.Forms.TextBox();
+            this.btn_OriginFile = new System.Windows.Forms.Button();
+            this.btnGoalFile = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxOrigin
+            // 
+            this.textBoxOrigin.Location = new System.Drawing.Point(33, 57);
+            this.textBoxOrigin.Multiline = true;
+            this.textBoxOrigin.Name = "textBoxOrigin";
+            this.textBoxOrigin.Size = new System.Drawing.Size(416, 25);
+            this.textBoxOrigin.TabIndex = 0;
+            // 
+            // textBoxGoalFile
+            // 
+            this.textBoxGoalFile.Location = new System.Drawing.Point(33, 125);
+            this.textBoxGoalFile.Multiline = true;
+            this.textBoxGoalFile.Name = "textBoxGoalFile";
+            this.textBoxGoalFile.Size = new System.Drawing.Size(416, 25);
+            this.textBoxGoalFile.TabIndex = 1;
+            // 
+            // btn_OriginFile
+            // 
+            this.btn_OriginFile.Location = new System.Drawing.Point(483, 59);
+            this.btn_OriginFile.Name = "btn_OriginFile";
+            this.btn_OriginFile.Size = new System.Drawing.Size(75, 26);
+            this.btn_OriginFile.TabIndex = 2;
+            this.btn_OriginFile.Text = "源文件";
+            this.btn_OriginFile.UseVisualStyleBackColor = true;
+            this.btn_OriginFile.Click += new System.EventHandler(this.btn_OriginFile_Click);
+            // 
+            // btnGoalFile
+            // 
+            this.btnGoalFile.Location = new System.Drawing.Point(483, 125);
+            this.btnGoalFile.Name = "btnGoalFile";
+            this.btnGoalFile.Size = new System.Drawing.Size(75, 26);
+            this.btnGoalFile.TabIndex = 3;
+            this.btnGoalFile.Text = "目的地址";
+            this.btnGoalFile.UseVisualStyleBackColor = true;
+            this.btnGoalFile.Click += new System.EventHandler(this.btnGoalFile_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(483, 205);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 47);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "复制";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(33, 220);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(416, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(623, 383);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGoalFile);
+            this.Controls.Add(this.btn_OriginFile);
+            this.Controls.Add(this.textBoxGoalFile);
+            this.Controls.Add(this.textBoxOrigin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxOrigin;
+        private System.Windows.Forms.TextBox textBoxGoalFile;
+        private System.Windows.Forms.Button btn_OriginFile;
+        private System.Windows.Forms.Button btnGoalFile;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- 复制文件时显示复制进度/Form1.cs
+++ 复制文件时显示复制进度/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,111 @@ namespace 复制文件时显示复制进度
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 打开路径浏览器选定要复制的地址
+        /// </summary>
+            string DefailtPath = "";//默认的路径
+        private void btn_OriginFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OriginFileDialog = new OpenFileDialog();//路径浏览器
+            if (DefailtPath != "")//记录上次点击时的路径
+            {
+                OriginFileDialog.FileName = DefailtPath;
+            }
+
+            if (OriginFileDialog.ShowDialog()== DialogResult.OK)
+            {
+                DefailtPath= OriginFileDialog.FileName;
+                textBoxOrigin.Text = OriginFileDialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// 选定复制的目的地址
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnGoalFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog GoalFoldDialog = new FolderBrowserDialog();
+            if (DefailtPath != "")//记录上次点击时的路径
+            {
+                GoalFoldDialog.SelectedPath = DefailtPath;
+            }
+
+            if (GoalFoldDialog.ShowDialog()== DialogResult.OK)//如果选定了路径之后
+            {
+                DefailtPath = GoalFoldDialog.SelectedPath;
+                textBoxGoalFile.Text = GoalFoldDialog.SelectedPath;
+            }
+        }
+
+        string toFile;
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            var toFile=textBoxOrigin.Text.Substring(textBoxOrigin.Text.LastIndexOf("\\")+1) ;//截取文件名
+            toFile =textBoxGoalFile.Text+ "\\"+toFile;//文件地址
+            CopyFile(textBoxOrigin.Text,toFile,10000,progressBar1);
+        }
+
+        public void CopyFile(string formerFile,string toFile,int sectSize,ProgressBar progressBar1)
+        {
+            progressBar1.Value = 0;//进度条的显示数据归0
+            progressBar1.Minimum = 0;
+            FileStream fileToCreate = new FileStream(toFile,FileMode.Create);//创建空文件
+            fileToCreate.Close();
+            fileToCreate.Dispose();
+            FileStream formerFileOpen = new FileStream(formerFile,FileMode.Open,FileAccess.Read);//读取文件
+            FileStream toFileOpen = new FileStream(toFile,FileMode.Append,FileAccess.Write);//给现存文件打开
+            int max = Convert.ToInt32(Math.Ceiling((double)formerFileOpen.Length/(double)sectSize));//文件的长度除以分次复制的次数即为每一次的长度
+            Console.WriteLine(max);
+            progressBar1.Maximum = max;
+            if (sectSize < formerFileOpen.Length)
+            {
+                byte[] buffer = new byte[sectSize];
+                int copied = 0;//记录已经复制的大小
+                int FileSize;//复制文件的大小
+                while ((int)(formerFileOpen.Length - sectSize) >= copied)
+                {
+                    FileSize = formerFileOpen.Read(buffer, 0, sectSize);
+                    formerFileOpen.Flush();
+                    toFileOpen.Write(buffer, 0, sectSize);
+                    toFileOpen.Flush();
+                    toFileOpen.Position = formerFileOpen.Position;
+                    copied += FileSize;
+                    progressBar1.Value++;
+                }
+                //处理剩余的文件
+                int left = (int)formerFileOpen.Length - copied;
+                FileSize = formerFileOpen.Read(buffer, 0, left);
+                formerFileOpen.Flush();
+                toFileOpen.Write(buffer, 0, left);
+                toFileOpen.Flush();
+                progressBar1.Value = progressBar1.Maximum;
+            }
+            else
+            {
+                byte[] buffer = new byte[sectSize];
+                formerFileOpen.Read(buffer,0,(int)formerFileOpen.Length);
+                formerFileOpen.Flush();
+                toFileOpen.Write(buffer, 0, (int)formerFileOpen.Length);
+                toFileOpen.Flush();
+            }
+            formerFileOpen.Flush();
+            formerFileOpen.Close();
+            formerFileOpen.Dispose();
+            toFileOpen.Flush();
+            toFileOpen.Close();
+            toFileOpen.Dispose();
+            if (MessageBox.Show("复制完成") == DialogResult.OK)
+            {
+                progressBar1.Value = 0;
+                textBoxGoalFile.Text = "";
+                textBoxOrigin.Text = "";
+            }
         }
     }
 }
